8fe8f45024be87affae258a43b3ab0c63de4e34a
Ekaterina Nikitina <katyanic@mail.ru>
2016-06-24 16:06:34 +0300

    Added graph & alg

--- src/root/Graph.java
+++ src/root/Graph.java
@@ -2,4 +2,22 @@ package root;
 
 public class Graph {
 
+	private int numberVertex;
+	private int matrix[][];
+
+	public Graph(int number) {
+		// по-хорошему, здесь нужно считать из файла либо получить иначе
+		numberVertex = number;
+		matrix = new int[numberVertex][numberVertex];
+	}
+
+	protected void solve() {
+		for (int k = 0; k < numberVertex; k++) {
+			for (int i = 0; i < numberVertex; i++) {
+				for (int j = 0; j < numberVertex; j++) {
+					matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+				}
+			}
+		}
+	}
 }
